160f662d4f945b214fb5f502fa0582518b80de14
noveboi <cptgiorgos03@gmail.com>
2024-01-15 22:46:00 +0200

    Added VS Project

---
+++ Ergasia_Final/ViewModels/ShellViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ergasia_Final.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        private string _firstName = "Mary";
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        private string _lastName = "Dimi";
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+                NotifyOfPropertyChange();
+            }
+        }
+    }
+}
