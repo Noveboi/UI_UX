d62585e951a551b644369dd0cbdf978d0ffd41bd
noveboi <cptgiorgos03@gmail.com>
2024-01-18 18:34:23 +0200

    added .gitignore

--- Ergasia_Final/obj/Debug/net6.0-windows/App.g.i.cs
+++ Ergasia_Final/obj/Debug/net6.0-windows/App.g.i.cs
@@ -47,13 +47,13 @@ namespace Ergasia_Final {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Ergasia_Final;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Ergasia_Final;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -67,7 +67,7 @@ namespace Ergasia_Final {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public static void Main() {
             Ergasia_Final.App app = new Ergasia_Final.App();
             app.InitializeComponent();
--- Ergasia_Final/obj/Debug/net6.0-windows/Views/DJView.g.i.cs
+++ Ergasia_Final/obj/Debug/net6.0-windows/Views/DJView.g.i.cs
@@ -47,13 +47,13 @@ namespace Ergasia_Final.Views {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Ergasia_Final;component/views/djview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Ergasia_Final;V1.0.0.0;component/views/djview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\DJView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -63,7 +63,7 @@ namespace Ergasia_Final.Views {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
