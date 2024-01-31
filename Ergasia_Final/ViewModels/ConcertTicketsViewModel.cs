b953cd90ade19adb21106b60361135bf49c6c9f5
noveboi <cptgiorgos03@gmail.com>
2024-01-31 17:03:26 +0200

    added functionality to UpCon

--- Ergasia_Final/ViewModels/ConcertTicketsViewModel.cs
+++ Ergasia_Final/ViewModels/ConcertTicketsViewModel.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Ergasia_Final.ViewModels
 {
-    public class ConcertTicketsViewModel
+    public class ConcertTicketsViewModel : Screen
     {
+        private string _test;
+
+        public string Test
+        {
+            get { return _test; }
+            set 
+            { 
+                _test = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public ConcertTicketsViewModel(string sender)
+        {
+            _test = sender;
+        }
     }
 }
--- Ergasia_Final/ViewModels/UpConViewModel.cs
+++ Ergasia_Final/ViewModels/UpConViewModel.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Ergasia_Final.ViewModels
 {
@@ -13,6 +15,12 @@ namespace Ergasia_Final.ViewModels
         public UpConViewModel(IEventAggregator eventAggregator)
         {
             _eventAggregator = eventAggregator;
+        }
+
+        // Methods
+        public void GoToTickets(Button sender)
+        {
+            _eventAggregator.PublishOnUIThreadAsync(new ConcertTicketsViewModel(sender.Name));
         }
     }
 }
