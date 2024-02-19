c247895451e12b01a0221200a574010a898f72ee
noveboi <cptgiorgos03@gmail.com>
2024-02-19 10:51:19 +0200

    added online help page (replaced help.html) + minor style changes

---
+++ Ergasia_Final/help/script.js
@@ -0,0 +1,13 @@
+window.onload = function () { 
+    var expanders = document.getElementsByClassName("menu-item")
+    for (var i = 0; i < expanders.length; i++) {
+        var expander = expanders[i]
+        expander.addEventListener("click", function () {
+            if (expander.classList.contains("active")) {
+                expander.classList.remove("active")
+            } else {
+                expander.classList.add("active")
+            }
+        });
+    }
+}
