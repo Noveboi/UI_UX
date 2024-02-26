ca0d97c5cf3646f8eeb01a75ebcb5d9e7dc9bb60
noveboi <cptgiorgos03@gmail.com>
2024-02-26 14:27:18 +0200

    finished improvements to e-pay (closes #34)

--- Ergasia_Final/ViewModels/EPayServiceViewModel.cs
+++ Ergasia_Final/ViewModels/EPayServiceViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using Caliburn.Micro;
 using Ergasia_Final.Utilities;
+using Microsoft.Xaml.Behaviors.Media;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,7 +25,10 @@ namespace Ergasia_Final.ViewModels
         // Fields
         private bool _isCalendarOpen = false;
         private string _dateDisplay = string.Empty;
-        private string _price;
+		private string _cvc = string.Empty;
+		private string _cardNumber = string.Empty;
+		private string _fullName = string.Empty;
+		private string _price;
         private IEventAggregator _eventAggregator;
 
         // Properties
@@ -39,19 +45,72 @@ namespace Ergasia_Final.ViewModels
                 NotifyOfPropertyChange();
             }
         }
-        public string DateDisplay
+        public string ExpirationDate
         {
             get => _dateDisplay;
             set
             {
-                _dateDisplay = value;
-                NotifyOfPropertyChange();
+                if (value.All(c => char.IsDigit(c) || c.Equals('/')))
+                {
+                    Set(ref _dateDisplay, value);
+                    ValidExpirationDate = IsValidExpirationDate(value);
+                    NotifyOfPropertyChange(nameof(ValidExpirationDate));
+                }
             }
         }
 
-        // Methods
-        public void Pay()
+		public string Cvc
         {
+            get => _cvc;
+            set 
+            {
+                if (value.All(char.IsDigit) && value.Length <= 3)
+                {
+                    Set(ref _cvc, value);
+                    ValidCVC = IsValidCVC(value);
+                    NotifyOfPropertyChange(nameof(ValidCVC));
+                }
+            }
+        }
+		public string CardNumber
+        {
+            get => _cardNumber;
+            set
+            {
+                if (value.All(chr => char.IsDigit(chr) || char.IsWhiteSpace(chr))       // Allow only digits and whitespaces
+                    && value.Where(c => !char.IsWhiteSpace(c)).ToArray().Length <= 16)  // Ensure that digits are 16 or less
+                {
+                    Set(ref _cardNumber, value);
+                    ValidCardNumber = IsValidCardNumber(value);
+                    NotifyOfPropertyChange(nameof(ValidCardNumber));
+                }
+			}
+        }
+		public string FullName 
+        { 
+            get => _fullName; 
+            set
+            {
+                Set(ref _fullName, value);
+                ValidFullName = value.Length > 0;
+                NotifyOfPropertyChange(nameof(ValidFullName));
+            }
+        }
+
+        public bool ValidFullName { get; set; }
+        public bool ValidCardNumber { get; set; }
+        public bool ValidExpirationDate { get; set; }
+        public bool ValidCVC { get; set; }
+
+		// Methods
+		public void Pay()
+        {
+            if (!IsValidCVC(Cvc) || !IsValidCardNumber(CardNumber) || FullName.Length == 0 || !IsValidExpirationDate(ExpirationDate))
+            {
+                MessageBox.Show("Please fill in valid information!", "Invalid Card Details", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
             MessageBox.Show("Payment successful!\nReturning to Main Menu", "Success!", MessageBoxButton.OK, MessageBoxImage.Information);
             _eventAggregator.PublishOnUIThreadAsync("epay to main");
         }
@@ -59,8 +118,23 @@ namespace Ergasia_Final.ViewModels
         public void OnMonthPicked(MonthPickerSelectionChangedEventArgs e)
         {
             string dateString = $"{e.Month}/{e.Year}";
-            DateDisplay = dateString;
+            ExpirationDate = dateString;
             IsCalendarOpen = false;
         }
-    }
+
+        private bool IsValidCVC(string cvcText)
+        {
+            return cvcText.All(char.IsDigit) && cvcText.Length == 3; 
+        }
+        private bool IsValidCardNumber(string cardNumberText)
+        {
+            return cardNumberText.All(chr => char.IsDigit(chr) || char.IsWhiteSpace(chr))
+            && cardNumberText.Where(c => !char.IsWhiteSpace(c)).ToArray().Length == 16;
+        }
+        private bool IsValidExpirationDate(string expDateText)
+        {
+            Regex regex = new Regex(@"^([1-9]|(10|11|12))\/2[0-1]([0-9]{2})$");
+            return regex.IsMatch(expDateText);
+        }
+	}
 }
