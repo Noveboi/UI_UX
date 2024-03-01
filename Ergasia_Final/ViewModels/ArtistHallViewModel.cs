2ac3d7d59b2e7f85bb65a2e43d55ceb735dafce6
noveboi <cptgiorgos03@gmail.com>
2024-03-01 15:49:37 +0200

    styled artisthall buttons + code docs

--- Ergasia_Final/ViewModels/ArtistHallViewModel.cs
+++ Ergasia_Final/ViewModels/ArtistHallViewModel.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Navigation;
@@ -15,11 +16,11 @@ namespace Ergasia_Final.ViewModels
     public class ArtistHallViewModel : Screen
     {
 
-        public Uri ArtistBackground { get; private set; }
-        public VideoPaketoModel Concert1 { get; set; }
-        public VideoPaketoModel Concert2 { get; set; }
-        public VideoPaketoModel Interview { get; set; }
-        public VideoPaketoModel Cover { get; set; }
+        public Uri? ArtistBackground { get; private set; }
+        public VideoPaketoModel? Concert1 { get; set; }
+        public VideoPaketoModel? Concert2 { get; set; }
+        public VideoPaketoModel? Interview { get; set; }
+        public VideoPaketoModel? Cover { get; set; }
 
         /// <param name="artistName">Determines what content to render based on the artist's name given</param>
         public ArtistHallViewModel(string artistName)
@@ -28,6 +29,7 @@ namespace Ergasia_Final.ViewModels
             SetNavigateUris(artistName);
         }
 
+        // Sets the Image background of the main Grid
         private void SetArtistBackground(string artistName)
         {
             if (artistName == "taylor")
@@ -44,8 +46,10 @@ namespace Ergasia_Final.ViewModels
             }
         } 
 
-
-
+        /// <summary>
+        /// Instantiates the <see cref="VideoPaketoModel"/> models based on the <paramref name="artistName"/> parameter
+        /// </summary>
+        /// <param name="artistName"></param>
         private void SetNavigateUris(string artistName)
         {
             if (artistName == "taylor")
@@ -56,12 +60,27 @@ namespace Ergasia_Final.ViewModels
                     VideoLink = new Uri("https://www.youtube.com/watch?v=P5JLMp08GC0")
                 };
             }
+            if (artistName == "m83")
+            {
+
+            }
+            if (artistName == "altj")
+            {
+
+            }
         }
 
-        public void OnRequestNavigate(Button source)
+        public static void OnRequestNavigate(Button source)
         {
-            Hyperlink href = source.Content as Hyperlink;
-            Process process = new Process()
+            Hyperlink? href = source.Content as Hyperlink;
+
+            if (href is null || href?.NavigateUri is null)
+            {
+                MessageBox.Show("Video not available!", "No link", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var process = new Process()
             {
                 StartInfo = new ProcessStartInfo(href.NavigateUri.AbsoluteUri)
             };
